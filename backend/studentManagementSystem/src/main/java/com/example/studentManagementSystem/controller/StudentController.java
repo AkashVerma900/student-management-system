9e525ca3ac6b0db5c1f7f3211db57712624640d3
Akash Verma <avakashverma900@gmail.com>
2026-06-04 12:27:24 +0530

    Day2 setup ready or oops concepts

---
+++ backend/studentManagementSystem/src/main/java/com/example/studentManagementSystem/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.studentManagementSystem.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.studentManagementSystem.model.Student;
+
+@RestController
+@RequestMapping("/student")
+@CrossOrigin(origins = "https://localhost:5173")
+public class StudentController {
+
+    @GetMapping
+    public Student getStudent() {
+
+        return new Student(
+                1,
+                "Akash",
+                "Verma"
+        );
+    }
+}
