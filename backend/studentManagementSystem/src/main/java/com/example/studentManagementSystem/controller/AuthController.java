93814f88e4c58fb00b897823d4f7d8357c2c9143
Akash Verma <avakashverma900@gmail.com>
2026-06-15 12:00:50 +0530

    Day 9 Spring Security Basics and JWT, OAuth2 done

---
+++ backend/studentManagementSystem/src/main/java/com/example/studentManagementSystem/controller/AuthController.java
@@ -0,0 +1,25 @@
+package com.example.studentManagementSystem.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.studentManagementSystem.dto.LoginRequestDTO;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173")
+public class AuthController {
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequestDTO dto) {
+    
+	   if(dto.getUsername().equals("admin") && dto.getPassword().equals("admin")){
+            return "JWT_TOKEN";
+        }
+
+        return "Invalid Credentials";
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ function App(){
     // {"id":1,"name":"Akash","course":"Computer Science"}
     const [students, setStudents] = useState([])
     const [count, setCount] = useState(0)
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
     const getStudentData = async()=>{
         const response = await fetch('http://localhost:8080/student');
         const data = await response.json();
@@ -13,22 +15,43 @@ function App(){
         const count = await response.json();
         setCount(count);
     }
+    const login = async()=>{
+        const response = await fetch('http://localhost:8080/auth/login',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({username,password}),
+        });
+        alert('Login successful');
+    }
     return(
-        <div>
-            <h1 align='center'>Welcome to the App!</h1>
-            <h3 align='center'>This the student Data</h3>
-            <center><button onClick={getStudentData}>Get Student Data</button></center>
-            <center><button onClick={fetchTotalStudentCount}>Get Student Count</button></center>
-            <p>Total students: {count}</p>
-            <ul>
-                {students.map((student) => (
-                    <li key={student.id}>
-                        <h2>{student.name}</h2>
-                        <h2>{student.course}</h2>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        //<div>
+           
+        //     <center><button onClick={getStudentData}>Get Student Data</button></center>
+        //     <center><button onClick={fetchTotalStudentCount}>Get Student Count</button></center>
+        //     <p>Total students: {count}</p>
+        //     <ul>
+        //         {students.map((student) => (
+        //             <li key={student.id}>
+        //                 <h2>{student.name}</h2>
+        //                 <h2>{student.course}</h2>
+        //             </li>
+        //         ))}
+        //     </ul>
+        // </div>
+
+    <div>
+        <h1 align='center'>Welcome to the App!</h1>
+        <h3 align='center'>This the student Data</h3>
+
+        <input placeholder="Username" onChange={(e)=>setUsername(e.target.value)} />
+
+        <input placeholder="Password" onChange={(e)=> setPassword(e.target.value)}/>
+
+        <button onClick={login}> Login </button>
+
+    </div>
         
     );
 }
